f25d0bfef40dc10460d880a558774d03b79ec613
dreamsaloni <103926119+dreamsaloni@users.noreply.github.com>
2023-04-12 12:41:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+status = "";
+
+function setup()
+{
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+  console.log("Model LOaded!!!");
+  staus = true;
+}
+
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+}
